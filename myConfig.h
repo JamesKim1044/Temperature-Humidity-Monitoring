0f67887a4a6599ee86449db4e86214f4d09c7b28
JamesKim1044 <56524957+JamesKim1044@users.noreply.github.com>
2021-01-12 12:24:21 +0900

    myConfig.h

---
+++ myConfig.h
@@ -0,0 +1,23 @@
+#ifndef MYCONFIG_H
+#define MYCONFIG_H
+
+// Your AP ID and Password Here
+#define AP_SSID1  ""
+#define AP_PASS1  ""
+
+#define AP_SSID2  ""  // IF NULL ""
+#define AP_PASS2  ""  // IF NULL ""
+
+#define AP_SSID3  ""   // IF NULL ""
+#define AP_PASS3  ""  // IF NULL ""
+
+
+// MQTT Settings 
+#define SERVER     "SERVER"
+#define PORT       1883 // MQTT PORT
+
+#define USER       "USER ID"  
+#define PASS       "USER PASS"
+#define DEVICE     "ESP-{{ESP MAC}}" 
+
+#endif
